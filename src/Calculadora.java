a1b372789aa00fd69d76f2d074c565354545a888
MaiconL04 <maiconluis637@gmail.com>
2025-08-28 14:34:46 -0300

    Commit2 - addsubtração
    Adicionar subtração

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,5 +2,9 @@ public class Calculadora {
     public static int soma(int a, int b){
         return a + b;
     }
+    public static int subratia(int a, int b){
+        return a - b;
+    }
+
 
 }
