b9ff7171aa875d08fc858929a8d225dfd4207573
MaiconL04 <maiconluis637@gmail.com>
2025-08-28 15:31:10 -0300

    Commit3 - MultiDivisao
    Adicionar MultiplicaçãoDivisão

---
+++ src/Calculadora.java
@@ -0,0 +1,8 @@
+public class Calculadora {
+    public static int multiplication(int a, int b){
+        return a * b;
+    }
+    public static int Division(int a, int b){
+        return a / b;
+    }
+}
